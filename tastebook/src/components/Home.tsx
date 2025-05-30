849a0960291e75fa6ca7cad4c2f2c7c6cfedfd72
daorichard <98173508+daorichard@users.noreply.github.com>
2025-05-30 16:00:20 -0400

    added basic form

---
+++ tastebook/src/components/Home.tsx
@@ -0,0 +1,18 @@
+import RecipeForm from './RecipeForm';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Taste Book</h1>
+
+      <p>Capture recipes and the memories that go with them</p>
+
+      <button>Add a recipe</button>
+      <RecipeForm
+        onSubmit={(data) => {
+          console.log('Recipe submitted:', data);
+        }}
+      ></RecipeForm>
+    </main>
+  );
+}
